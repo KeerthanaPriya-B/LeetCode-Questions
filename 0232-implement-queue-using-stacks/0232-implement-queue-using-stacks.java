2e08fe18f3151701a54c34f33b834191d77746de
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-11 01:24:03 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (96.80%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -10,7 +10,10 @@ class MyQueue {
     
     public void push(int x) {
         
+        //TC: push-O(1)  pop & top most of the time takes TC: O(1) and such cases called amortized complexity but it is O(N) anyhow.
         s1.push(x);
+        
+        //commented lines of program has TC: push-O(N) & pop-O(1)
         // while(!s1.isEmpty()) s2.push(s1.pop());        
         // s1.push(x);       
         // while(!s2.isEmpty()) s1.push(s2.pop());
