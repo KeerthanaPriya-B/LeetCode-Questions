59ed2003dfc2ae1691608c4beac9f9134a617fbb
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-10 10:19:36 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (31.89%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -9,19 +9,27 @@ class MyQueue {
     }
     
     public void push(int x) {
-        while(!s1.isEmpty()) s2.push(s1.pop());
+//         while(!s1.isEmpty()) s2.push(s1.pop());        
+//         s1.push(x);       
+//         while(!s2.isEmpty()) s1.push(s2.pop());
         
         s1.push(x);
-        
-        while(!s2.isEmpty()) s1.push(s2.pop());
     }
     
     public int pop() {
-        return s1.pop();
+        //return s1.pop();
+        while(!s1.isEmpty()) s2.push(s1.pop());
+        int popped = s2.pop();
+        while(!s2.isEmpty()) s1.push(s2.pop());
+        return popped;
     }
     
     public int peek() {
-        return s1.peek();
+        //return s1.peek();
+        while(!s1.isEmpty()) s2.push(s1.pop());
+        int peekEle = s2.peek();
+        while(!s2.isEmpty()) s1.push(s2.pop());
+        return peekEle;
     }
     
     public boolean empty() {
