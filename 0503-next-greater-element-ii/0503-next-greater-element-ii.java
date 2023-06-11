7454919efe7cf43d5a66221873f7534c28ef5c63
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-11 12:48:27 +0530

    Time: 43 ms (27.44%), Space: 44.3 MB (62.67%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>(); //O(N)
         int n = nums.length;
-        int[] ans = new int[n];
+        int[] ans = new int[n]; //O(N)
         
-        
+        //this way of doing includes, for(i=n-1 -> 0) and remove %n
         // for(int i=n-1; i>=0; i--) 
         //     stack.push(nums[i]);
         
+        
+        
+        //TC: O(2N + 2N) approx O(N)  SC: we usually won't consider space used to stote result, so the SC: O(N)
         for(int i=2*n-1; i>=0; i--)
         {
             while(!stack.isEmpty() && stack.peek() <= nums[i%n]) 
