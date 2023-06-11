25f8b67eee7aa757f4f8ea78a9c953f1fb9dae21
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-11 12:40:00 +0530

    Time: 42 ms (31.42%), Space: 44.7 MB (41.24%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -6,20 +6,22 @@ class Solution {
         int[] ans = new int[n];
         
         
-        for(int i=n-1; i>=0; i--) stack.push(nums[i]);
+        // for(int i=n-1; i>=0; i--) 
+        //     stack.push(nums[i]);
         
-        for(int i=n-1; i>=0; i--){
-            //if(nums[i] < stack.peek())
-            while(!stack.isEmpty() && stack.peek() <= nums[i]) stack.pop();
+        for(int i=2*n-1; i>=0; i--)
+        {
+            while(!stack.isEmpty() && stack.peek() <= nums[i%n]) 
+                stack.pop();
             
-            if(!stack.isEmpty()) {
-                ans[i] = stack.peek();
-            }else{
-                ans[i] = -1;
-            }
-            
-            stack.push(nums[i]);
+            if(!stack.isEmpty()) 
+                ans[i%n] = stack.peek();
+            else
+                ans[i%n] = -1;
+              
+            stack.push(nums[i%n]);
         }
+        
         return ans;
     }
 }
