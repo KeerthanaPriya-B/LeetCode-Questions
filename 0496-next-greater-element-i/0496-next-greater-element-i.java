56fb0e0b3665bf4178e10d160e00ddbca4e6f7c7
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-12 15:15:26 +0530

    Time: 4 ms (75.68%), Space: 43.4 MB (33.13%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -4,22 +4,35 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         Stack<Integer> s = new Stack<>();
         
-        for(int i=nums2.length-1; i>=0; i--)
-        {
-            while(!s.isEmpty() && s.peek() < nums2[i])
-                s.pop();
-            
-            if(s.isEmpty())
-                map.put(nums2[i], -1);
-            else
-                map.put(nums2[i], s.peek());
-            
-            s.push(nums2[i]);
+        //traversing from front to end approach 
+        for(int n: nums2){
+             while(!s.isEmpty() && s.peek() < n)
+                 map.put(s.pop(), n); 
+            s.push(n);
         }
-        
         for(int i=0; i<nums1.length; i++)
-            nums1[i] = map.get(nums1[i]);
-         
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        
         return nums1;
+        
+        
+         //traversing from end to front approach 
+//         for(int i=nums2.length-1; i>=0; i--)
+//         {
+//             while(!s.isEmpty() && s.peek() < nums2[i])
+//                 s.pop();
+            
+//             if(s.isEmpty())
+//                 map.put(nums2[i], -1);
+//             else
+//                 map.put(nums2[i], s.peek());
+            
+//             s.push(nums2[i]);
+//         }
+        
+//         for(int i=0; i<nums1.length; i++)
+//             nums1[i] = map.get(nums1[i]);
+         
+//         return nums1;
     }
 }
