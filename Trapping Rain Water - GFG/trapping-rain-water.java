3d0be9c583c276e760c759faf3e964e27f3c277e
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-13 05:06:04 +0530

    Added solution - LeetHub

--- Trapping Rain Water - GFG/trapping-rain-water.java
+++ Trapping Rain Water - GFG/trapping-rain-water.java
@@ -41,28 +41,6 @@ class Solution{
     static long trappingWater(int arr[], int n) { 
         // Your code here
         
-        // int left = 0, right = n - 1;
-        // long res = 0;
-        // int maxLeft = 0, maxRight = 0;
-        // while (left <= right) {
-        //     if (height[left] <= height[right]) {
-        //         if (height[left] >= maxLeft) {
-        //             maxLeft = height[left];
-        //         } else {
-        //             res += maxLeft - height[left];
-        //         }
-        //         left++;
-        //     } else {
-        //         if (height[right] >= maxRight) {
-        //             maxRight = height[right];
-        //         } else {
-        //             res += maxRight - height[right];
-        //         }
-        //         right--;
-        //     }
-        // }
-        // return res;
-        
         int left = 0, right = n-1, Lmax = 0, Rmax = 0;
         long waterTrapped = 0;
         
