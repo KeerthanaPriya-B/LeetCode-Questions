cd7d253b00271bac9f915f974f49877caab71d60
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-20 16:34:19 +0530

    Added solution - LeetHub

--- Bitonic Point - GFG/bitonic-point.java
+++ Bitonic Point - GFG/bitonic-point.java
@@ -35,19 +35,24 @@ public class Main {
 class Solution {
     int findMaximum(int[] A, int n) {
         // code here
+        
+        //recording: 19th june Binary search 2 time(2:30:00)
+		//TC: O(log n)  SC: O(1)
+		
+		
         int start = 0, end = A.length;
 
 		while(start <= end) {
 			int mid = (start+end)/2;
 
-			if(mid == 0) {  //cases like 1 2 3 4 5
+			if(mid == 0) {  //cases like 4 3 2 1 and 1 2 3 4 
 				if(A[mid] > A[mid+1])
 					return A[mid];
 				else
 					//move right
 					start = mid+1;
 			}
-			else if(mid == A.length-1) {  // cases like 5 4 3 2 1 
+			else if(mid == A.length-1) {  // cases like 1 2 3 4 and 4 3 2 1
 				if(A[mid] > A[mid-1])
 					return A[mid];
 				else
