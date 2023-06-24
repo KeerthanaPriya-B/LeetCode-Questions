c568bdd3d92df20d2ac49b10ffc83d1a8f7a9119
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-24 14:58:36 +0530

    Time: 0 ms (100.00%), Space: 45 MB (36.27%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,31 +1,19 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    public int search(int[] arr, int k) {
         
-        return helper(nums, target, 0, nums.length-1);
-    }
-    
-    static int helper(int[] nums, int target, int start, int end){
+        int n = arr.length;
+        int low = 0, high = n-1;
         
-        if(start > end)  return -1;
-        
-        int mid = start + (end-start)/2;
-        
-        if(target == nums[mid]) return mid;
-        
-        if(target < nums[mid])  
-            return helper(nums, target, start, mid-1);
-        
-        return helper(nums, target, mid+1, end);
+        while(low <= high) {
+            int mid = (low+high)/2;
+            
+            if(arr[mid] == k)
+                return mid;
+            else if(arr[mid] > k)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return -1;
     }
 }
-
-
-// if(st>end)return -1;
-    
-//     int mid = st+(end-st)/2;
-    
-//     if(nums[mid] == target) return mid;
-    
-//     if(nums[mid] < target) return sum(nums,mid+1,end, target);
-    
-//     else return sum(nums,st,mid-1, target); 
