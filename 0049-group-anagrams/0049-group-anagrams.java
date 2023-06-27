b21b276a6aa44f91c3e8ea0271192e745ae18654
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-27 12:49:00 +0530

    Time: 10 ms (39.17%), Space: 47.4 MB (24.02%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] wordArr) {
+        
+        HashMap<String, List<String>> groups = new HashMap<>();
+		List<List<String>> res = new ArrayList<>();
+
+		//TC: O(N)
+		for(String wrd: wordArr) {
+			
+			//TC: O(M)
+			String code = generateCode(wrd);
+
+			if(groups.containsKey(code)) {
+				List<String> list = groups.get(code);
+				list.add(wrd);
+				groups.put(code, list);
+			}
+			else {
+				List<String> list = new ArrayList<>();
+				list.add(wrd);
+				groups.put(code, list);
+			}
+		}
+        for(String code: groups.keySet()) {
+            List<String> list = groups.get(code);
+            res.add(list);   
+        }
+        return res;
+    }
+    
+    public String generateCode(String wrd) {
+
+		int[] freqArr = new int[26];
+
+		for(char ch: wrd.toCharArray()) 
+			freqArr[ch - 'a']++;
+
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<26; i++) {
+			if(freqArr[i] > 0) {
+				sb.append((char)('a' + i));
+				sb.append(freqArr[i]);
+			}
+		}
+		return sb.toString();
+	}
+}
