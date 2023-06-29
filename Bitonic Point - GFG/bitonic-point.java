82c1a85834ee05deb59657f1935802ba2f96c1de
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-06-29 22:29:42 +0530

    Added solution - LeetHub

--- Bitonic Point - GFG/bitonic-point.java
+++ Bitonic Point - GFG/bitonic-point.java
@@ -33,45 +33,22 @@ public class Main {
 
 
 class Solution {
-    int findMaximum(int[] A, int n) {
+    int findMaximum(int[] arr, int n) {
         // code here
         
         //recording: 19th june Binary search 2 time(2:30:00)
 		//TC: O(log n)  SC: O(1)
 		
-		
-        int start = 0, end = A.length;
-
-		while(start <= end) {
-			int mid = (start+end)/2;
-
-			if(mid == 0) {  //cases like 4 3 2 1 and 1 2 3 4 
-				if(A[mid] > A[mid+1])
-					return A[mid];
-				else
-					//move right
-					start = mid+1;
-			}
-			else if(mid == A.length-1) {  // cases like 1 2 3 4 and 4 3 2 1
-				if(A[mid] > A[mid-1])
-					return A[mid];
-				else
-					//move left
-					end = mid-1;
-			}
-			else {
-				if(A[mid-1] < A[mid] && A[mid] > A[mid+1])
-					return A[mid];
-				else if(A[mid-1] < A[mid])
-					//increasing side
-					//move towards right
-					start = mid+1;
-				else
-					//decreasing side
-					//move towards left
-					end = mid-1;
-			}
-			
+		int l=0, h=n-1;
+		while(l <= h) {
+		    int mid = (l+h)/2;
+		    
+		    if((mid==0 || arr[mid-1] < arr[mid]) && (mid==n-1 || arr[mid+1] < arr[mid]))
+		        return arr[mid];
+            else if(arr[mid-1] > arr[mid])
+                h = mid-1;
+            else
+                l = mid+1;
 		}
 		return -1;
     }
