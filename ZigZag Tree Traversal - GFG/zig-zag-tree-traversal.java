bfe34d3ae29fccc98da4df814f44ebcb6969fb05
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-06 02:50:36 +0530

    Added solution - LeetHub

---
+++ ZigZag Tree Traversal - GFG/zig-zag-tree-traversal.java
@@ -0,0 +1,167 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.util.HashMap;
+import java.io.*;
+
+    class Node
+    {
+        int data;
+        Node left,right;
+        Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+    }
+		
+public class GFG2
+{
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void inorder(Node root)
+	{
+	    if(root==null)
+	    return;
+	    inorder(root.left);
+	    System.out.print(root.data);
+	    inorder(root.right);
+	}
+     /* Drier program to test above functions */
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    GFG g = new GFG();
+			
+			    ArrayList<Integer> res = g.zigZagTraversal(root) ;
+			    for (int i = 0; i < res.size (); i++)
+			        System.out.print (res.get (i) + " ");
+			    System. out. println();  
+    			
+                t--;
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}*/
+
+class GFG
+{
+    //Function to store the zig zag order traversal of tree in a list.
+	ArrayList<Integer> zigZagTraversal(Node root)
+	{
+	    //Add your code here.
+	    
+	    //recording: 29th june binary search 2 time(2:37:00)
+		//TC: O(N)  SC: O(2^H) atmax we can have all the nodes 
+		//of one level in queue(full binary tree)
+		
+		ArrayList<Integer> res = new ArrayList<>();
+		
+		Queue<Node> que = new ArrayDeque<>();
+		que.add(root);
+		int level = 0;
+		
+		while(que.size() != 0) {
+			ArrayList<Integer> al = new ArrayList<>();
+			int size = que.size();
+	
+			while(size-- > 0) {
+				Node node = que.remove();
+				al.add(node.data);
+				
+				if(node.left != null)
+					que.add(node.left);
+				
+				if(node.right != null)
+					que.add(node.right);
+			}
+			
+			if(level%2 == 0) {
+				for(int i=0; i<al.size(); i++)
+					res.add(al.get(i));
+			} else {
+				for(int i=al.size()-1; i>=0; i--)
+					res.add(al.get(i));
+			}
+			level++;
+		}
+		return res;
+	}
+}
