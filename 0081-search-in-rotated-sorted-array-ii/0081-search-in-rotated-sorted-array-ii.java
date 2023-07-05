b4c168b1b2e9c90e288377459e3e9f8209f0c6e2
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-05 21:05:02 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (48.20%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        
+        int l=0, h=nums.length-1;
+        
+        while(l <= h) {
+            int mid = (l+h)/2;
+            
+            //if target val is found
+            if(nums[mid] == target)
+                return true;
+            
+            //if all points to the duplicates
+            if(nums[l] == nums[mid] && nums[h] == nums[mid]) {
+                l++;
+                h--;
+            }
+            //checking whether it lies in the sorted region
+            else if(nums[l] <= nums[mid]) {
+                if(target >= nums[l] && target < nums[mid])
+                    h = mid -1;
+                else
+                    l = mid + 1;
+            }
+            else{
+                if(target > nums[mid] && target <= nums[h])
+                    l = mid + 1;
+                else
+                    h = mid - 1;
+            }
+                
+        }
+        return false;
+    }
+}
