abea5f88c512e23fe9028d0b4b7f9088f2900c9d
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-10 16:29:51 +0530

    Time: 15 ms (14.51%), Space: 44.2 MB (40.44%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -3,29 +3,27 @@ class Solution {
         
         Stack<Integer> s = new Stack<>();
         
-        for(int ele: asteroids){
-            if(ele>0)
-                s.push(ele);
-            else{
-                //stack.peek()-positive case
-                while(!s.isEmpty() && s.peek()>0 && s.peek()<Math.abs(ele))
-                    s.pop();
-                
-                //stack.peek()-empty & negative case
-                if(s.isEmpty() || s.peek()<0)
-                    s.push(ele);
-                
-                //stack.peek()-equal case
-                else if(!s.isEmpty() && s.peek()==Math.abs(ele))
-                    s.pop();
+        for(int i=0; i<asteroids.length; i++) {
+            while(!s.isEmpty() && asteroids[s.peek()] > 0 && asteroids[i] < 0 && Math.abs(asteroids[i]) > asteroids[s.peek()]) {
+               s.pop(); 
             }
+            
+            if(!s.isEmpty() && asteroids[i] < 0  && asteroids[s.peek()] < 0)
+                s.push(i);
+            else if(s.isEmpty() && asteroids[i] < 0 )
+                s.push(i);
+            else if(asteroids[i] > 0)
+                s.push(i);
+            else if(!s.isEmpty() && asteroids[s.peek()] == Math.abs(asteroids[i]))
+                s.pop();
+            
+            
         }
+        int[] a = new int[s.size()];
         
-        int[] res = new int[s.size()];
+        for(int i=a.length-1; i>=0; i--)
+            a[i] = asteroids[s.pop()];
         
-        for(int i=s.size()-1; i>=0; i--)
-            res[i] = s.pop();
-        
-        return res;
+        return a;
     }
 }
