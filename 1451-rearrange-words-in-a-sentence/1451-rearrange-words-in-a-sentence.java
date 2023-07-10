7701e6ed5baab5dbaf683c0d686bd1df3eb23d90
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-10 17:42:38 +0530

    Time: 32 ms (43.55%), Space: 45.2 MB (30.24%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
@@ -2,33 +2,28 @@ class Solution {
     public String arrangeWords(String text) {
        
         TreeMap<Integer, ArrayList<String>> map = new TreeMap<>();
+        StringBuilder sb = new StringBuilder();
         
         text = text.toLowerCase();
         String words[] = text.split(" ");
         
         for(String wrd: words) {
             int len = wrd.length();
+            ArrayList<String> list = new ArrayList<>();
             
-            if(map.containsKey(len)) {
-                ArrayList<String> list = map.get(len);
-                list.add(wrd);
-                map.put(len, list);
-            } else {
-                ArrayList<String> list = new ArrayList<>();
-                list.add(wrd);
-                map.put(len, list);
-            }
+            if(map.containsKey(len)) 
+                list = map.get(len);
+            
+            list.add(wrd);
+            map.put(len, list);
         }
         
-        StringBuilder sb = new StringBuilder();
         for(ArrayList<String> list : map.values()) {
             for(String wrd: list)
                 sb.append(wrd+" ");
         }
-        String ans = sb.toString();
-        char[] ch = ans.toCharArray();
-        // for(char c: ch)
-        //     System.out.print(c+" ");
+        
+        char[] ch = sb.toString().toCharArray();
         ch[0] = (ch[0] >= 97 && ch[0] <= 122) ? (char)(ch[0]-32) : ch[0];
         
         return new String(ch).trim();
