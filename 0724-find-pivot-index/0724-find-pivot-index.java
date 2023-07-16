7334b5102649e7ded16841490497abecd5834cf7
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-16 21:10:08 +0530

    Time: 1 ms (100.00%), Space: 44 MB (81.86%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,26 +1,42 @@
 class Solution {
-    public int pivotIndex(int[] nums) {
+    public int pivotIndex(int[] arr) {
         
         
         //watched video from Techdose Utube Channel
         
-        int n = nums.length;
-        if(n == 1)  return 0;
-        
-        int sum[] = new int[n];
+        //SC: O(1)
+        int n = arr.length;
         int tot = 0;
+        for(int ele: arr)  tot += ele;
         
-        for(int i=0; i<n; i++) {
-            tot += nums[i];
-            sum[i] = tot;
+        int left = 0;
+        for(int i=0; i<n; i++) 
+        {
+            tot -=arr[i];
+            if(tot == left) return i;
+            left += arr[i];
         }
         
-        for(int i=0; i<n; i++) {
-            int Lsum = sum[i] - nums[i];
-            int Rsum = tot - sum[i];
+        return -1;
+        
+        //SC: O(N)
+//         int n = nums.length;
+//         if(n == 1)  return 0;
+        
+//         int sum[] = new int[n];
+//         int tot = 0;
+        
+//         for(int i=0; i<n; i++) {
+//             tot += nums[i];
+//             sum[i] = tot;
+//         }
+        
+//         for(int i=0; i<n; i++) {
+//             int Lsum = sum[i] - nums[i];
+//             int Rsum = tot - sum[i];
             
-            if(Lsum == Rsum)  return i;
-        }
-        return -1;   
+//             if(Lsum == Rsum)  return i;
+//         }
+//         return -1;   
     }
 }
