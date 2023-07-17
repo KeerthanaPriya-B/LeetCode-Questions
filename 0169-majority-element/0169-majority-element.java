d5802d068bb59a50fbaca54b5facd6f643f34872
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-17 22:18:28 +0530

    Time: 1 ms (99.80%), Space: 49.2 MB (8.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,42 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
+        
+        //Moore's Voting Algorithm
+        //striver's code
+        
+        
         int times = nums.length/2;
+        int val = 0, cnt = 0;
         
-        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            if(cnt == 0) {
+                val = nums[i];
+                cnt = 1;
+            }
+            else if(nums[i] == val) cnt++;
+            else cnt--;
+        }
         
-        for(int ele: nums)
-            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        cnt = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(val == nums[i])  cnt++;
+        }
         
-       for(int key: map.keySet()) { 
-           if(map.get(key) > times)
-               return key;
-       }
-        return 0;
+        if(cnt > times) return val;
+        return -1;
+        
+        
+        
+//         HashMap<Integer, Integer> map = new HashMap<>();
+        
+//         for(int ele: nums)
+//             map.put(ele, map.getOrDefault(ele, 0) + 1);
+        
+//        for(int key: map.keySet()) { 
+//            if(map.get(key) > times)
+//                return key;
+//        }
+//         return 0;
     }
 }
