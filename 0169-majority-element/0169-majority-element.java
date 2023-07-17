7235b4d35e25c882157dc3d38621b9d110959464
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-17 21:44:56 +0530

    Time: 12 ms (37.30%), Space: 47.3 MB (69.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -8,11 +8,9 @@ class Solution {
         for(int ele: nums)
             map.put(ele, map.getOrDefault(ele, 0) + 1);
         
-       for(int key: map.keySet()) {
-           //if(map.containsKey(key)) 
-               if(map.get(key) > times)
-                   return key;
-           //}
+       for(int key: map.keySet()) { 
+           if(map.get(key) > times)
+               return key;
        }
         return 0;
     }
