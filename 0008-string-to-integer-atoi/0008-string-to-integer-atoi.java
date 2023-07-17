46ed42c279878662a885d425d5cc8ea2a591b323
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-17 13:50:05 +0530

    Time: 2 ms (64.57%), Space: 41.7 MB (37.53%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,25 +1,23 @@
 class Solution {
     public int myAtoi(String s) { 
-        int i = 0, n = s.length(), sign = 1;;
+        int i = 0, n = s.length(), sign = 1, num = 0, maxVal = Integer.MAX_VALUE, minVal = Integer.MIN_VALUE;
         
-        while(i < n && s.charAt(i) == ' ')
-            i++;
-        
+        //for space
+        while(i < n && s.charAt(i) == ' ')  i++;
         if(i == n) return 0;
         
+        //for sign
         if(s.charAt(i) == '-' || s.charAt(i) == '+'){
             sign = (s.charAt(i) == '+') ? 1 : -1;
             i++;
         }
-        
         if(i == n) return 0;
         
-        int num = 0, maxVal = Integer.MAX_VALUE, minVal = Integer.MIN_VALUE;
-        
-        while(i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') 
-        {
+        //for integers
+        while(i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9')  {
             int digit = s.charAt(i) - '0';
             
+            //checking whether the num is in the limit or not
             if(num > maxVal/10 || (num == maxVal/10 && digit > maxVal%10))
                 return (sign == 1) ? maxVal : minVal;
             
