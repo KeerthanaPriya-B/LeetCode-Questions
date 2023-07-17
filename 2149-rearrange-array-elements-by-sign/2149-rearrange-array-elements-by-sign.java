26e82edf4010b871e07b73fa8bff3b33af3c1878
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-18 00:10:20 +0530

    Time: 17 ms (14.46%), Space: 79.8 MB (83.79%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,18 +1,60 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         
-        int[] res = new int[nums.length];
-        int positive = 0, negative = 1;
+        int n = nums.length;
         
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] > 0) {
-                res[positive] = nums[i];
-                positive += 2;
-            } else {
-                res[negative] = nums[i];
-                negative += 2;
+        int[] res = new int[n];
+        ArrayList<Integer> posi = new ArrayList<>();
+        ArrayList<Integer> nega = new ArrayList<>();
+        
+        
+        for(int i=0; i<n; i++) {
+            if(nums[i] > 0) posi.add(nums[i]);
+            if(nums[i] < 0) nega.add(nums[i]);
+        }
+        
+        if(posi.size() > nega.size()) {
+            for(int i=0; i<nega.size(); i++) {
+                res[2*i] = posi.get(i);
+                res[2*i+1] = nega.get(i);
             }
+            int idx = nega.size()*2;
+            for(int j=nega.size(); j<posi.size(); j++)
+                res[idx++] = posi.get(j);
+        } 
+        else {
+            for(int i=0; i<posi.size(); i++) {
+                res[2*i] = posi.get(i);
+                res[2*i+1] = nega.get(i);
+            }
+            int idx = posi.size()*2;
+            for(int j=posi.size(); j<nega.size(); j++)
+                res[idx++] = nega.get(j);
         }
         return res;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int[] res = new int[nums.length];
+//         int positive = 0, negative = 1;
+        
+//         for(int i=0; i<nums.length; i++) {
+//             if(nums[i] > 0) {
+//                 res[positive] = nums[i];
+//                 positive += 2;
+//             } else {
+//                 res[negative] = nums[i];
+//                 negative += 2;
+//             }
+//         }
+//         return res;
     }
 }
