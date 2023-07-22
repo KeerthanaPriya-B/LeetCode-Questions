b754e550a172a50be787635a6a25149a6ec3ae94
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-22 20:51:36 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -33,46 +33,77 @@ class GFG {
 
 
 class Solution {
-    class Pair {
-        int vertex;
-        int parent;
-        Pair(int vertex, int parent) {
-            this.vertex = vertex;
-            this.parent = parent;
-        }
-    }
     
-    public boolean detect(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
+//************************* using dfs **********************************
+    
+    public boolean detect(int node, int parent, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
         vis[node] = true;
-        Queue<Pair> q = new ArrayDeque<>();
-        q.add(new Pair(node, -1));
         
-        while(!q.isEmpty()) {
-            Pair pair = q.remove();
-            
-            for(int neighbour: adj.get(pair.vertex)) {
-                if(!vis[neighbour]) {
-                    q.add(new Pair(neighbour, pair.vertex));
-                    vis[neighbour] = true;
-                }
-                //if already visited node is not parent, we found cycle
-                else if(neighbour != pair.parent){
+        for(int neighbour: adj.get(node)) {
+            if(!vis[neighbour]) {
+                if(detect(neighbour, node, adj, vis))  
                     return true;
-                }
+            }
+            else if(neighbour != parent){
+                return true;
             }
         }
         return false;
     }
     
-    // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
         boolean[] vis = new boolean[V];
         
         for(int i=0; i<V; i++) {
             if(!vis[i])
-                if(detect(i, adj, vis))  return true;
+                if(detect(i, -1, adj, vis))  return true;
         }
         return false;
     }
+    
+//************************* using bfs **********************************
+    
+    // class Pair {
+    //     int vertex;
+    //     int parent;
+    //     Pair(int vertex, int parent) {
+    //         this.vertex = vertex;
+    //         this.parent = parent;
+    //     }
+    // }
+    
+    // public boolean detect(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
+    //     vis[node] = true;
+    //     Queue<Pair> q = new ArrayDeque<>();
+    //     q.add(new Pair(node, -1));
+        
+    //     while(!q.isEmpty()) {
+    //         Pair pair = q.remove();
+            
+    //         for(int neighbour: adj.get(pair.vertex)) {
+    //             if(!vis[neighbour]) {
+    //                 q.add(new Pair(neighbour, pair.vertex));
+    //                 vis[neighbour] = true;
+    //             }
+    //             //if already visited node is not parent, we found cycle
+    //             else if(neighbour != pair.parent){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+    
+    // // Function to detect cycle in an undirected graph.
+    // public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+    //     // Code here
+    //     boolean[] vis = new boolean[V];
+        
+    //     for(int i=0; i<V; i++) {
+    //         if(!vis[i])
+    //             if(detect(i, adj, vis))  return true;
+    //     }
+    //     return false;
+    // }
 }
