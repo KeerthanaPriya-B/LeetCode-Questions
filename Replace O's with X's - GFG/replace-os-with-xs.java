619be19481afa4ff6d4a3c343d5e83c44da1db1d
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-24 19:50:51 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.java
+++ Replace O's with X's - GFG/replace-os-with-xs.java
@@ -40,7 +40,7 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-    static void dfs(int r, int c, int n, int m, char a[][], boolean[][] vis, char[][] ans) {
+    static void dfs(int r, int c, int n, int m, char a[][], boolean[][] vis) {
         vis[r][c] = true;
         
         int[] rowArr = {-1, 0, 1, 0};
@@ -51,7 +51,7 @@ class Solution{
             int col = c + colArr[i];
             
             if(row>=0 && row<n && col>=0 && col<m && !vis[row][col] && a[row][col] == 'O') {
-                dfs(row, col, n, m, a, vis, ans);
+                dfs(row, col, n, m, a, vis);
             }
         }
     }
@@ -60,23 +60,22 @@ class Solution{
     {
         // code here
         boolean[][] vis = new boolean[n][m];
-        char[][] ans = new char[n][m];
         
         for(int i=0; i<m; i++) {
             if(a[0][i] == 'O' && !vis[0][i]) {
-                dfs(0, i, n, m, a, vis, ans);
+                dfs(0, i, n, m, a, vis);
             }
             if(a[n-1][i] == 'O' && !vis[n-1][i]) {
-                dfs(n-1, i, n, m, a, vis, ans);
+                dfs(n-1, i, n, m, a, vis);
             }
         }
         
         for(int i=0; i<n; i++) {
             if(a[i][0] == 'O' && !vis[i][0]) {
-                dfs(i, 0, n, m, a, vis, ans);
+                dfs(i, 0, n, m, a, vis);
             }
             if(a[i][m-1] == 'O' && !vis[i][m-1]) {
-                dfs(i, m-1, n, m, a, vis, ans);
+                dfs(i, m-1, n, m, a, vis);
             }
         }
         
