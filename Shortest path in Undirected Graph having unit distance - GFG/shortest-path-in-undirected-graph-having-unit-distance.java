5b9410c493b788e47715a7fd81d20b12cceaf176
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-07-31 15:46:12 +0530

    Added solution - LeetHub

---
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public int[] shortestPath(int[][] edges,int n,int m ,int src) {
+        // Code here
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(); 
+        for(int i = 0;i<n;i++) {
+            adj.add(new ArrayList<>()); 
+        }
+        for(int i = 0;i<m;i++) {
+            adj.get(edges[i][0]).add(edges[i][1]); 
+            adj.get(edges[i][1]).add(edges[i][0]); 
+        }
+        
+        int[] dis = new int[n];
+        for(int i=0; i<n; i++)  dis[i] = Integer.MAX_VALUE;
+        dis[src] = 0;
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(src);
+        
+        while(!q.isEmpty()) {
+            int node = q.remove();
+            
+            for(int edge: adj.get(node)) {
+                if(dis[node] + 1 < dis[edge]) {
+                    dis[edge] = dis[node] + 1;
+                    q.add(edge);
+                }
+            }
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(dis[i] == Integer.MAX_VALUE) dis[i] = -1;
+        }
+        return dis;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
