0ee49367655036bb5146b4251ce796a3d791cb98
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-08-01 13:38:09 +0530

    Added solution - LeetHub

---
+++ Maximum Consecutive Ones - GFG/maximum-consecutive-ones.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] nums = IntArray.input(br, n);
+            
+            
+            int k;
+            k = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.longestOnes(n, nums, k);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int longestOnes(int n, int[] nums, int k) {
+        // code here
+        int si = 0;
+        int ei = 0;
+        int dk = 0;
+        int maxlen = 0;
+        
+        while(ei < n) {
+            //if(nums[ei] == 1) 
+            if(nums[ei] == 0) dk++;
+            
+            while(dk > k) {
+                int len = ei - si;
+                maxlen = Math.max(maxlen, len);
+                
+                if(nums[si] == 0) dk--;
+                si++;
+            }
+            ei++;
+        }
+        int len = ei - si;
+        maxlen = Math.max(maxlen, len);
+        
+        return maxlen;
+    }
+}
+        
