d24ea7a62f9a95bc58887f412a72b0908c0fb056
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-08-01 11:40:57 +0530

    Time: 3 ms (27.37%), Space: 45.2 MB (32.30%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -9,7 +9,7 @@ class Solution {
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(isConnected[i][j] == 1) {
+                if(isConnected[i][j] == 1 && i!=j) {
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
