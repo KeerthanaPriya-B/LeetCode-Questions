5a5d604e99123d2b30fbf5d4dcfef4ab5d567a1d
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-08-03 19:04:51 +0530

    Time: 4 ms (18.91%), Space: 45.2 MB (33.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,39 +1,49 @@
 class Solution {
+    public void bfs(int n, List<List<Integer>> adj, boolean[] vis) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(n);
+        vis[n] = true;
+        
+        while(!q.isEmpty()) {
+            int node = q.remove();
+            
+            for(int edge: adj.get(node)) {
+                if(!vis[edge]) {
+                    vis[edge] = true;
+                    q.add(edge);}
+            }
+        }
+    }
     public int findCircleNum(int[][] isConnected) {
         
         int n = isConnected.length;
+        List<List<Integer>> adj = new ArrayList<>();
         
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i=0; i<n; i++) 
             adj.add(new ArrayList<>());
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(isConnected[i][j] == 1 && i!=j) {
+                if(i!=j && isConnected[i][j] == 1) {
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
             }
         }
         boolean[] vis = new boolean[n];
-        int cnt = 0;
+        int cnt=0;
         for(int i=0; i<n; i++) {
             if(!vis[i]) {
                 cnt++;
-                dfs(i, vis,adj);
+                bfs(i, adj, vis);
             }
-                
         }
         return cnt;
     }
-    public void dfs(int node, boolean[] vis, ArrayList<ArrayList<Integer>> adj) {
-        vis[node] = true;
-        for(int edge: adj.get(node)) {
-            if(!vis[edge])
-                dfs(edge, vis, adj);
-        }
-    }
 }
+
+
+
 
 
 
