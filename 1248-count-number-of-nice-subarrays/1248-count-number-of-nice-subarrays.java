1b8e8d7c426aa0d66f9c245431e41610d2eb85d8
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-08-04 16:22:15 +0530

    Time: 13 ms (60.17%), Space: 52.3 MB (67.69%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,23 +1,32 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return cntsubarr(nums, k) - cntsubarr(nums, k-1);
+        
+        //exactly k odd = atmost k odds - atmost k-1 odds
+        return cntSubArr(nums, k) - cntSubArr(nums, k-1);
     }
-    public int cntsubarr(int[]nums, int k) {
+    
+    public int cntSubArr(int[] nums, int k) {
         int n = nums.length;
         int si = 0;
         int ei = 0;
-        int dk = 0;
         int cnt = 0;
         
+        //depending on this we shrink the window
+        int odds = 0;
+        
         while(ei < n) {
-            if(nums[ei]%2 != 0) dk++;
+            if(nums[ei]%2 != 0) odds++;
             
-            while(dk > k) {
-                if(nums[si]%2 != 0) dk--;
+            //if odd cnt exceeding k, shrink
+            while(odds > k) {
+                //if start index is odd decrement odd cnt
+                if(nums[si]%2 != 0) odds--;
                 si++;
             }
-            
+            //update cnt of subarrays
             cnt += ei-si+1;
+            
+            //expand the window
             ei++;
         }
         return cnt;
