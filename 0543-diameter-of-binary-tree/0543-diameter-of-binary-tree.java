975f843baba4485712dbd389665534dec78ced44
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-09-08 12:49:52 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (65.36%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,23 +14,31 @@
  * }
  */
 class Solution {
-    int maxi = 0;
-    public int diameterOfBinaryTree(TreeNode root) {
+//     int maxi = 0;
+     public int diameterOfBinaryTree(TreeNode root) {
+    
+            int[] maxi = new int[1];
+            findHeight(root, maxi);
+            return maxi[0];
+//         if(root == null) return 0;
+        
+//         int LHeight = findHeight(root.left);
+//         int RHeight = findHeight(root.right);
+        
+//         maxi = Math.max(maxi, LHeight + RHeight);
+        
+//         diameterOfBinaryTree(root.left);
+//         diameterOfBinaryTree(root.right);
+        
+//         return maxi;
+     }
+    public int findHeight(TreeNode root, int[] maxi) {
         if(root == null) return 0;
         
-        int LHeight = findHeight(root.left);
-        int RHeight = findHeight(root.right);
+        int LST = findHeight(root.left, maxi);
+        int RST = findHeight(root.right, maxi);
         
-        maxi = Math.max(maxi, LHeight + RHeight);
-        
-        diameterOfBinaryTree(root.left);
-        diameterOfBinaryTree(root.right);
-        
-        return maxi;
-    }
-    public int findHeight(TreeNode root) {
-        if(root == null) return 0;
-        
-        return 1 + Math.max(findHeight(root.left), findHeight(root.right));
+        maxi[0] = Math.max(maxi[0], LST + RST);
+        return 1 + Math.max(LST, RST);
     }
 }
