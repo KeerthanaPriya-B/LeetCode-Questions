6d9739754da610c62e0cfeabb6e236a1febf963d
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-09-11 15:04:08 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (39.28%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,32 +2,34 @@ class Solution {
     public void rotate(int[][] mat) {
         
         int n = mat.length;
-        int m = mat[0].length;
         
         //transpose
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(i < j) {
-                    int temp = mat[i][j];
-                    mat[i][j] = mat[j][i];
-                    mat[j][i] = temp;
-                }
+        for(int i=0; i<n-1; i++) {
+            for(int j=i+1; j<n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
             }
         }
         
         //reversing rows
         for(int i=0; i<n; i++) {
-            int[] arr = mat[i];
-            
-            int j=0;
-            int k=m-1;
-            while(j < k) {
-                int temp = arr[j];
-                arr[j] = arr[k];
-                arr[k] = temp;
-                j++;
-                k--;
+            for(int j=0; j<n/2; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][n-j-1];
+                mat[i][n-j-1] = temp;
             }
+//             int[] arr = mat[i];
+            
+//             int j=0;
+//             int k=m-1;
+//             while(j < k) {
+//                 int temp = arr[j];
+//                 arr[j] = arr[k];
+//                 arr[k] = temp;
+//                 j++;
+//                 k--;
+//             }
         }
     }
 }
